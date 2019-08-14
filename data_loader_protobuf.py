963f33f8a2a452e03477b6f515b8124076d4b468
Maurice Houben <mhouben@int1.bullx>
2019-08-14 13:08:48 +0200

    implemented protobuf reader, fixed logging to create separate log files for each, added try catches to see why errors happen and make the program not stop immediately, also added train_format and valid_format parameters that are default at .png and .jpg, respectively. data_loader_protobuf and train_new are not used and probably can be removed.

--- data_loader_protobuf.py
+++ data_loader_protobuf.py
@@ -1,5 +1,4 @@
 import os
-import traceback
 import numpy as np
 import pandas as pd
 from skimage import io
@@ -7,33 +6,18 @@ import torch
 from torchvision import transforms
 from torch.utils.data import Dataset
 import image_pb2
-from pathlib import Path
+
 
 class TripletFaceDataset(Dataset):
 
-    def __init__(self, root_dir, csv_name, num_triplets, format, transform = None):
+    def __init__(self, root_dir, csv_name, num_triplets, transform = None):
         
         self.root_dir          = root_dir
         self.df                = pd.read_csv(csv_name)
         self.num_triplets      = num_triplets
         self.transform         = transform
         self.training_triplets = self.generate_triplets(self.df, self.num_triplets)
-        self.format            = format
     
-    def get_image(self, person, image):
-        for person_file in os.listdir(self.root_dir):
-            #print("hit line 25")
-            if os.path.isfile(Path(self.root_dir+person_file)) and person_file == person+".pid":
-                #print("hit line 27")
-                f = open(Path(self.root_dir+person_file), "rb")
-                p = image_pb2.Person()
-                p.ParseFromString(f.read())
-                f.close()
-                for i in p.images:
-                    #print("hit line 33")
-                    if i.name == image:
-                        print(i.name)
-                        return i
     
     @staticmethod
     def generate_triplets(df, num_triplets):
@@ -88,56 +72,45 @@ class TripletFaceDataset(Dataset):
         
         return triplets
     
-    @staticmethod
-    def deserialize_image(image_bytes):
-        deserialized_bytes = np.frombuffer(image_bytes, dtype=np.int8)
-        deserialized_x = np.reshape(deserialized_bytes, newshape=(250, 250))
-        return deserialized_x
+    def get_image(person, image):
+        for person_file in os.listdir(dir_path):
+            if os.path.isfile(Path(dir_path+person_file)) and person_file == person+".pid":
+                f = open(Path(dir_path+person_file), "rb")
+                p = image_pb2.Person()
+                p.ParseFromString(f.read())
+                f.close()
+                for i in p.images:
+                    if i.name == image:
+                        return i
     
     def __getitem__(self, idx):
-        keep = {}
-        try:
-            anc_id, pos_id, neg_id, pos_class, neg_class, pos_name, neg_name = self.training_triplets[idx]
-            
-            anc_img   = os.path.join(self.root_dir, str(pos_name), str(anc_id) + self.format)
-            pos_img   = os.path.join(self.root_dir, str(pos_name), str(pos_id) + self.format)
-            neg_img   = os.path.join(self.root_dir, str(neg_name), str(neg_id) + self.format)
-            
-            keep = {anc_img, pos_img, neg_img}
-            
-            anc_id = str(anc_id) + self.format
-            pos_id = str(pos_id) + self.format
-            neg_id = str(neg_id) + self.format
-            
-            print("maurice test", pos_name, anc_id, self.get_image(pos_name, anc_id))
-            
-            anc_img = self.get_image(pos_name, anc_id).contents
-            pos_img = self.get_image(pos_name, pos_id).contents
-            neg_img = self.get_image(neg_name, neg_id).contents
-            
-            anc_img = self.deserialize_image(anc_img)
-            pos_img = self.deserialize_image(pos_img)
-            neg_img = self.deserialize_image(neg_img)
-            
-            
-            
-            #anc_img   = io.imread(anc_img, plugin='imageio')
-            #pos_img   = io.imread(pos_img, plugin='imageio')
-            #neg_img   = io.imread(neg_img, plugin='imageio')
+        
+        anc_id, pos_id, neg_id, pos_class, neg_class, pos_name, neg_name = self.training_triplets[idx]
+        
+        anc_img   = os.path.join(self.root_dir, str(pos_name), str(anc_id) + '.png')
+        pos_img   = os.path.join(self.root_dir, str(pos_name), str(pos_id) + '.png')
+        neg_img   = os.path.join(self.root_dir, str(neg_name), str(neg_id) + '.png')
+        
+        anc_img = get_image(pos_name, anc_id + '.png')
+        pos_img = get_image(pos_name, pos_id + '.png')
+        neg_img = get_image(neg_name, neg_id + '.png')
+        
+        anc_img   = io.imread(anc_img, plugin='imageio')
+        pos_img   = io.imread(pos_img, plugin='imageio')
+        neg_img   = io.imread(neg_img, plugin='imageio')
+        
+        
 
-            pos_class = torch.from_numpy(np.array([pos_class]).astype('long'))
-            neg_class = torch.from_numpy(np.array([neg_class]).astype('long'))
-            
-            sample = {'anc_img': anc_img, 'pos_img': pos_img, 'neg_img': neg_img, 'pos_class': pos_class, 'neg_class': neg_class}
+        pos_class = torch.from_numpy(np.array([pos_class]).astype('long'))
+        neg_class = torch.from_numpy(np.array([neg_class]).astype('long'))
+        
+        sample = {'anc_img': anc_img, 'pos_img': pos_img, 'neg_img': neg_img, 'pos_class': pos_class, 'neg_class': neg_class}
 
-            if self.transform:
-                sample['anc_img'] = self.transform(sample['anc_img'])
-                sample['pos_img'] = self.transform(sample['pos_img'])
-                sample['neg_img'] = self.transform(sample['neg_img'])
-        except:
-            print("traceback: ", traceback.format_exc())
-            sample = {'exception': True}
-            print("exception occurred: ", keep)
+        if self.transform:
+            sample['anc_img'] = self.transform(sample['anc_img'])
+            sample['pos_img'] = self.transform(sample['pos_img'])
+            sample['neg_img'] = self.transform(sample['neg_img'])
+            
         return sample
     
     
@@ -149,8 +122,7 @@ class TripletFaceDataset(Dataset):
 def get_dataloader(train_root_dir,     valid_root_dir, 
                    train_csv_name,     valid_csv_name, 
                    num_train_triplets, num_valid_triplets, 
-                   batch_size,         num_workers,
-                   train_format,       valid_format):
+                   batch_size,         num_workers):
     
     data_transforms = {
         'train': transforms.Compose([
@@ -167,12 +139,10 @@ def get_dataloader(train_root_dir,     valid_root_dir,
         'train' : TripletFaceDataset(root_dir     = train_root_dir,
                                      csv_name     = train_csv_name,
                                      num_triplets = num_train_triplets,
-                                     format       = train_format,
                                      transform    = data_transforms['train']),
         'valid' : TripletFaceDataset(root_dir     = valid_root_dir,
                                      csv_name     = valid_csv_name,
                                      num_triplets = num_valid_triplets,
-                                     format       = valid_format,
                                      transform    = data_transforms['valid'])}
 
     dataloaders = {
--- train.py
+++ train.py
@@ -1,4 +1,8 @@
+import os
+import traceback
 import numpy as np
+from datetime import datetime
+import time
 import argparse
 import torch
 import torch.nn as nn
@@ -15,6 +19,12 @@ from data_loader import TripletFaceDataset, get_dataloader
 
 parser = argparse.ArgumentParser(description = 'Face Recognition using Triplet Loss')
 
+parser.add_argument('--train_format', type=str, 
+                    help='Format of images for training set', default='.png')
+parser.add_argument('--valid_format', type=str, 
+                    help='Format of images for validation set', default='.jpg')
+parser.add_argument('--logs_base_dir', type=str, 
+                    help='Directory where to write event logs.', default='./log/')
 parser.add_argument('--start-epoch', default = 0, type = int, metavar = 'SE',
                     help = 'start epoch (default: 0)')
 parser.add_argument('--num-epochs', default = 200, type = int, metavar = 'NE',
@@ -43,38 +53,46 @@ parser.add_argument('--train-csv-name', default = './datasets/test_vggface2.csv'
                     help = 'list of training images')
 parser.add_argument('--valid-csv-name', default = './datasets/lfw.csv', type = str,
                     help = 'list of validtion images')
+                    
 
 args    = parser.parse_args()
 device  = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 l2_dist = PairwiseDistance(2)
+subdir = datetime.strftime(datetime.now(), '%Y%m%d-%H%M%S')
+log_dir = os.path.join(os.path.expanduser(args.logs_base_dir), subdir)
 
 
 def main():
-    
+    print("Start date and time: ", time.asctime(time.localtime(time.time())))
+    print("arguments: ", args)
     model     = FaceNetModel(embedding_size = args.embedding_size, num_classes = args.num_classes).to(device)
     optimizer = optim.Adam(model.parameters(), lr = args.learning_rate)
     scheduler = lr_scheduler.StepLR(optimizer, step_size = 50, gamma = 0.1)
     
+    if not os.path.isdir(log_dir):  # Create the log directory if it doesn't exist
+        os.makedirs(log_dir)
+    
     if args.start_epoch != 0:
-        checkpoint = torch.load('./log/checkpoint_epoch{}.pth'.format(args.start_epoch-1))
+        checkpoint = torch.load('{}/checkpoint_epoch{}.pth'.format(log_dir, args.start_epoch-1))
         model.load_state_dict(checkpoint['state_dict'])
     
     for epoch in range(args.start_epoch, args.num_epochs + args.start_epoch):
-        
+        t = time.time()
         print(80 * '=')
         print('Epoch [{}/{}]'.format(epoch, args.num_epochs + args.start_epoch - 1))
     
         data_loaders, data_size = get_dataloader(args.train_root_dir,     args.valid_root_dir,
                                                  args.train_csv_name,     args.valid_csv_name,
                                                  args.num_train_triplets, args.num_valid_triplets,   
-                                                 args.batch_size,         args.num_workers)
+                                                 args.batch_size,         args.num_workers,
+                                                 args.train_format,       args.valid_format)
 
-        train_valid(model, optimizer, scheduler, epoch, data_loaders, data_size)
-
+        train_valid(model, optimizer, scheduler, epoch, data_loaders, data_size, t)
+        print("duration of epoch ", epoch, ": ", time.time()-t, " seconds")
     print(80 * '=')
-
+    print("End date and time: ", time.asctime(time.localtime(time.time())))
     
-def train_valid(model, optimizer, scheduler, epoch, dataloaders, data_size):
+def train_valid(model, optimizer, scheduler, epoch, dataloaders, data_size, start_time):
     
     for phase in ['train', 'valid']:
 
@@ -88,82 +106,91 @@ def train_valid(model, optimizer, scheduler, epoch, dataloaders, data_size):
             model.eval()
     
         for batch_idx, batch_sample in enumerate(dataloaders[phase]):
+            try:
+                if not 'exception' in batch_sample:
+                    anc_img = batch_sample['anc_img'].to(device)
+                    pos_img = batch_sample['pos_img'].to(device)
+                    neg_img = batch_sample['neg_img'].to(device)
                 
-            anc_img = batch_sample['anc_img'].to(device)
-            pos_img = batch_sample['pos_img'].to(device)
-            neg_img = batch_sample['neg_img'].to(device)
-        
-            pos_cls = batch_sample['pos_class'].to(device)
-            neg_cls = batch_sample['neg_class'].to(device)
-                            
-            with torch.set_grad_enabled(phase == 'train'):
-            
-                # anc_embed, pos_embed and neg_embed are encoding(embedding) of image
-                anc_embed, pos_embed, neg_embed = model(anc_img), model(pos_img), model(neg_img)
-            
-                # choose the hard negatives only for "training"
-                pos_dist = l2_dist.forward(anc_embed, pos_embed)
-                neg_dist = l2_dist.forward(anc_embed, neg_embed)
-            
-                all = (neg_dist - pos_dist < args.margin).cpu().numpy().flatten()
-                if phase == 'train':
-                    hard_triplets = np.where(all == 1)
-                    if len(hard_triplets[0]) == 0:
-                        continue
-                else:
-                    hard_triplets = np.where(all >= 0)
+                    pos_cls = batch_sample['pos_class'].to(device)
+                    neg_cls = batch_sample['neg_class'].to(device)
+                                    
+                    with torch.set_grad_enabled(phase == 'train'):
+                    
+                        # anc_embed, pos_embed and neg_embed are encoding(embedding) of image
+                        anc_embed, pos_embed, neg_embed = model(anc_img), model(pos_img), model(neg_img)
+                    
+                        # choose the hard negatives only for "training"
+                        pos_dist = l2_dist.forward(anc_embed, pos_embed)
+                        neg_dist = l2_dist.forward(anc_embed, neg_embed)
+                    
+                        all = (neg_dist - pos_dist < args.margin).cpu().numpy().flatten()
+                        if phase == 'train':
+                            hard_triplets = np.where(all == 1)
+                            if len(hard_triplets[0]) == 0:
+                                continue
+                        else:
+                            hard_triplets = np.where(all >= 0)
+                        
+                        anc_hard_embed = anc_embed[hard_triplets].to(device)
+                        pos_hard_embed = pos_embed[hard_triplets].to(device)
+                        neg_hard_embed = neg_embed[hard_triplets].to(device)
+                    
+                        anc_hard_img   = anc_img[hard_triplets].to(device)
+                        pos_hard_img   = pos_img[hard_triplets].to(device)
+                        neg_hard_img   = neg_img[hard_triplets].to(device)
+                    
+                        pos_hard_cls   = pos_cls[hard_triplets].to(device)
+                        neg_hard_cls   = neg_cls[hard_triplets].to(device)
+                    
+                        anc_img_pred   = model.forward_classifier(anc_hard_img).to(device)
+                        pos_img_pred   = model.forward_classifier(pos_hard_img).to(device)
+                        neg_img_pred   = model.forward_classifier(neg_hard_img).to(device)
+                    
+                        triplet_loss   = TripletLoss(args.margin).forward(anc_hard_embed, pos_hard_embed, neg_hard_embed).to(device)
                 
-                anc_hard_embed = anc_embed[hard_triplets].to(device)
-                pos_hard_embed = pos_embed[hard_triplets].to(device)
-                neg_hard_embed = neg_embed[hard_triplets].to(device)
-            
-                anc_hard_img   = anc_img[hard_triplets].to(device)
-                pos_hard_img   = pos_img[hard_triplets].to(device)
-                neg_hard_img   = neg_img[hard_triplets].to(device)
-            
-                pos_hard_cls   = pos_cls[hard_triplets].to(device)
-                neg_hard_cls   = neg_cls[hard_triplets].to(device)
-            
-                anc_img_pred   = model.forward_classifier(anc_hard_img).to(device)
-                pos_img_pred   = model.forward_classifier(pos_hard_img).to(device)
-                neg_img_pred   = model.forward_classifier(neg_hard_img).to(device)
-            
-                triplet_loss   = TripletLoss(args.margin).forward(anc_hard_embed, pos_hard_embed, neg_hard_embed).to(device)
-        
-                if phase == 'train':
-                    optimizer.zero_grad()
-                    triplet_loss.backward()
-                    optimizer.step()
-            
-                dists = l2_dist.forward(anc_embed, pos_embed)
-                distances.append(dists.data.cpu().numpy())
-                labels.append(np.ones(dists.size(0))) 
+                        if phase == 'train':
+                            optimizer.zero_grad()
+                            triplet_loss.backward()
+                            optimizer.step()
+                    
+                        dists = l2_dist.forward(anc_embed, pos_embed)
+                        distances.append(dists.data.cpu().numpy())
+                        labels.append(np.ones(dists.size(0))) 
 
-                dists = l2_dist.forward(anc_embed, neg_embed)
-                distances.append(dists.data.cpu().numpy())
-                labels.append(np.zeros(dists.size(0)))
-            
-                triplet_loss_sum += triplet_loss.item()
+                        dists = l2_dist.forward(anc_embed, neg_embed)
+                        distances.append(dists.data.cpu().numpy())
+                        labels.append(np.zeros(dists.size(0)))
+                    
+                        triplet_loss_sum += triplet_loss.item()
+            except:
+                #traceback.print_exc()
+                print("traceback: ", traceback.format_exc())
+                print("something went wrong with batch_idx: ", batch_idx, ", batch_sample:", batch_sample, ", neg_img batch_sample: ", batch_sample['neg_img'])
   
         avg_triplet_loss = triplet_loss_sum / data_size[phase]
         labels           = np.array([sublabel for label in labels for sublabel in label])
         distances        = np.array([subdist for dist in distances for subdist in dist])
-    
-        tpr, fpr, accuracy, val, val_std, far = evaluate(distances, labels)
-        print('  {} set - Triplet Loss       = {:.8f}'.format(phase, avg_triplet_loss))
-        print('  {} set - Accuracy           = {:.8f}'.format(phase, np.mean(accuracy)))
-   
-        with open('./log/{}_log_epoch{}.txt'.format(phase, epoch), 'w') as f:
-            f.write(str(epoch)             + '\t' +
-                    str(np.mean(accuracy)) + '\t' +
-                    str(avg_triplet_loss))
-            
-        if phase == 'train':
-            torch.save({'epoch': epoch,
-                        'state_dict': model.state_dict()},
-                        './log/checkpoint_epoch{}.pth'.format(epoch))
-        else:
-            plot_roc(fpr, tpr, figure_name = './log/roc_valid_epoch_{}.png'.format(epoch))
+        
+        nrof_pairs = min(len(labels), len(distances))
+        if nrof_pairs >= 10:
+            tpr, fpr, accuracy, val, val_std, far = evaluate(distances, labels)
+            print('  {} set - Triplet Loss       = {:.8f}'.format(phase, avg_triplet_loss))
+            print('  {} set - Accuracy           = {:.8f}'.format(phase, np.mean(accuracy)))
+            duration = time.time()-start_time
+       
+            with open('{}/{}_log_epoch{}.txt'.format(log_dir, phase, epoch), 'w') as f:
+                f.write(str(epoch)             + '\t' +
+                        str(np.mean(accuracy)) + '\t' +
+                        str(avg_triplet_loss)  + '\t' +
+                        str(duration))
+                
+            if phase == 'train':
+                torch.save({'epoch': epoch,
+                            'state_dict': model.state_dict()},
+                            '{}/checkpoint_epoch{}.pth'.format(log_dir, epoch))
+            else:
+                plot_roc(fpr, tpr, figure_name = '{}/roc_valid_epoch_{}.png'.format(log_dir, epoch))
 
 
 if __name__ == '__main__':
